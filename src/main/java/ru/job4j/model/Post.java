0ecfd4b20e41f4fb39b99eb233ce5a47f4c6f34f
Brux <okladno94@gmail.com>
2025-01-22 12:21:47 +0500

    Модель данных - Post. [#285211 #529572]

--- src/main/java/ru/job4j/model/Post.java
+++ src/main/java/ru/job4j/model/Post.java
@@ -39,8 +39,12 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
         return id == post.id && Objects.equals(link, post.link);
     }
@@ -52,12 +56,12 @@ public class Post {
 
     @Override
     public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", link='" + link + '\'' +
-                ", description='" + description + '\'' +
-                ", time=" + time +
-                '}';
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", time=" + time
+                + '}';
     }
 }
