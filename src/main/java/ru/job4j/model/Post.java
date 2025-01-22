10017dee3f6d275bca69dc68635fb799f0902613
Brux <okladno94@gmail.com>
2025-01-22 11:39:00 +0500

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/model/Post.java
@@ -0,0 +1,63 @@
+package ru.job4j.model;
+
+import java.util.Objects;
+
+public class Post {
+    private long id;
+    private String title;
+    private String link;
+    private String description;
+    private long time;
+
+    public Post(long id, String title, String link, String description, long time) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
