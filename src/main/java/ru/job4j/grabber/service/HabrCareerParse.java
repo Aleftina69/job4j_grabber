f9c4f746eb7dddf7b1592a4a9fb1b36fbabaa8d5
Brux <okladno94@gmail.com>
2025-02-04 14:25:46 +0500

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -14,32 +14,35 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int PAGE_COUNT = 5;
 
     @Override
     public List<Post> fetch() {
         var result = new ArrayList<Post>();
         try {
-            int pageNumber = 1;
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-            var connection = Jsoup.connect(fullLink);
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                var titleElement = row.select(".vacancy-card__title").first();
-                var linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK,
-                        linkElement.attr("href"));
-                var dataElement = row.select("time").first();
-                String date = dataElement.attr("datetime");
-                ZonedDateTime vacancyDateTime = ZonedDateTime.parse(date);
-                long timestamp = vacancyDateTime.toInstant().toEpochMilli();
-                var post = new Post();
-                post.setTitle(vacancyName);
-                post.setLink(link);
-                post.setTime(timestamp);
-                result.add(post);
-            });
+            for (int i = 0; i < PAGE_COUNT; i++) {
+                String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, i, SUFFIX);
+                var connection = Jsoup.connect(fullLink);
+                var document = connection.get();
+                var rows = document.select(".vacancy-card__inner");
+
+                rows.forEach(row -> {
+                    var titleElement = row.select(".vacancy-card__title").first();
+                    var linkElement = titleElement.child(0);
+                    String vacancyName = titleElement.text();
+                    String link = String.format("%s%s", SOURCE_LINK,
+                            linkElement.attr("href"));
+                    var dataElement = row.select("time").first();
+                    String date = dataElement.attr("datetime");
+                    ZonedDateTime vacancyDateTime = ZonedDateTime.parse(date);
+                    long timestamp = vacancyDateTime.toInstant().toEpochMilli();
+                    var post = new Post();
+                    post.setTitle(vacancyName);
+                    post.setLink(link);
+                    post.setTime(timestamp);
+                    result.add(post);
+                });
+            }
         } catch (IOException e) {
             LOGGER.error("When load page", e);
         }
