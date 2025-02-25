d5228670b368b8e4bb7a7d5c1976a77c2a8add55
Brux <okladno94@gmail.com>
2025-02-25 12:49:36 +0500

    Web [#289478 #533175]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,19 +2,18 @@ package ru.job4j;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.job4j.grabber.model.Post;
 import ru.job4j.grabber.service.Config;
 import ru.job4j.grabber.service.SchedulerManager;
 import ru.job4j.grabber.service.SuperJobGrab;
 import ru.job4j.grabber.service.Web;
 import ru.job4j.grabber.stores.JdbcStore;
 import ru.job4j.grabber.stores.Store;
+import ru.job4j.grabber.model.Post;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
@@ -24,9 +23,13 @@ public class Main {
                 config.get("db.username"),
                 config.get("db.password"))) {
             Store store = new JdbcStore(connection);
+
             var post = new Post();
             post.setTitle("Super Java Job");
+            post.setLink("link");
+            post.setDescription("description");
             store.save(post);
+
             var scheduler = new SchedulerManager();
             scheduler.init();
             scheduler.load(
@@ -34,6 +37,7 @@ public class Main {
                     SuperJobGrab.class,
                     store
             );
+
             new Web(store).start(Integer.parseInt(config.get("server.port")));
         } catch (SQLException e) {
             LOGGER.error("When creating a connection", e);
